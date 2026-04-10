c3b74d243f0bf144dc7f86f344815132301b50a4
User <user@example.com>
2026-04-11 02:45:22 +0530

    DB issue when deploying

--- src/main/java/com/project/userapi/UserApiApplication.java
+++ src/main/java/com/project/userapi/UserApiApplication.java
@@ -2,12 +2,9 @@ package com.project.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class UserApiApplication {
-    
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
     }
