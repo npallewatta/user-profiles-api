85ec9e716cfee7ad910e7de224527a392eae142e
User <user@example.com>
2026-04-11 02:21:43 +0530

    Excluded DB

--- src/main/java/com/project/userapi/UserApiApplication.java
+++ src/main/java/com/project/userapi/UserApiApplication.java
@@ -2,9 +2,12 @@ package com.project.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UserApiApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
     }
