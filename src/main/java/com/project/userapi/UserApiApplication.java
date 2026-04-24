2417bfb628901eadfb6b23ea64173d3f3a1e9801
User <user@example.com>
2026-04-24 18:45:45 +0530

    Excluded UserRepository.class

--- src/main/java/com/project/userapi/UserApiApplication.java
+++ src/main/java/com/project/userapi/UserApiApplication.java
@@ -3,7 +3,9 @@ package com.project.userapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.project.userapi.repository.UserRepository;
+
+@SpringBootApplication(exclude = UserRepository.class)
 public class UserApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
